39d3a1409ba0098c965069d8d43eb0931e47a996
Diego Garrido <di.garridop@duocuc.cl>
2026-04-15 12:06:40 -0400

    Se sube el framework inicial

---
+++ src/main/java/com/example/cat_alog/CatAlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.cat_alog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatAlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatAlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cat_alog/CatAlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cat_alog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatAlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
