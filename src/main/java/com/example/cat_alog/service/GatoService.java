fbf07140d0e269e1bbb8f3609dd5a3f5d25ac24f
TurboKitten <di.garridop@duocuc.cl>
2026-05-05 14:19:16 -0400

    Se llena el service para Gato y Usuario

--- src/main/java/com/example/cat_alog/service/GatoService.java
+++ src/main/java/com/example/cat_alog/service/GatoService.java
@@ -1,5 +1,38 @@
 package com.example.cat_alog.service;
 
+import com.example.cat_alog.model.Gato;
+import com.example.bibliotecaduoc.repository.GatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class GatoService {
 
+    @Autowired
+    private GatoRepository gatoRepository;
+
+    public List<Gato> getGatos(){
+        return gatoRepository.findAll();
+    }
+
+    public Gato saveGato(Gato gato){
+        return gatoRepository.save(gato);
+    }
+
+    public Gato getGatoId(int id){
+        return gatoRepository.findById(id).orElse(null);
+    }
+
+    public Gato updateGato(Gato gato){
+        if (!gatoRepository.existsById(gato.getId())){
+            return null;
+        }
+        return gatoRepository.save(gato);
+    }
+
+    public void deleteUsuario(int id){
+        gatoRepository.deteleById(id);
+    }
 }
---
+++ src/main/java/com/example/cat_alog/service/UsuarioService.java
@@ -0,0 +1,38 @@
+package com.example.cat_alog.service;
+
+import com.example.cat_alog.model.Usuario;
+import com.example.bibliotecaduoc.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> getUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario saveUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario getUsuarioId(int id){
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public Usuario updateUsuario(Usuario usuario){
+        if (!usuarioRepository.existsById(usuario.getId())){
+            return null;
+        }
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deleteUsuario(int id){
+        usuarioRepository.deteleById(id);
+    }
+}
